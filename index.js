53c770dc4ed253585bbdaa6fbd5ccee5f9d549c6
Jonatass06 <116910385+Jonatass06@users.noreply.github.com>
2023-08-20 15:10:27 +0000

    Separando os arquivos (4)

--- index.js
+++ index.js
@@ -12,39 +12,39 @@ document.getElementById("hint").addEventListener("click", e =>{
 })
 
 // pistas
-document.getElementById("pista1").addEventListener("click", e =>{
+document.getElementById("hint1").addEventListener("click", e =>{
   textHint.innerText = "Essa janela não deixa um clima agradável, está um pouco quente!";
   modal.style.display = "flex";
 })
 
-document.getElementById("pista2").addEventListener("click", e =>{
+document.getElementById("hint2").addEventListener("click", e =>{
   textHint.innerText = "Esse controle podia estar todo pintado se ficasse aqui por mais tempo!";
   modal.style.display = "flex";
 })
 
-document.getElementById("pista3a").addEventListener("click", e =>{
+document.getElementById("hint3-a").addEventListener("click", e =>{
   textHint.innerText = "A tinta desses canetões só não é parea para a próxima dica!";
   modal.style.display = "flex";
 })
-document.getElementById("pista3b").addEventListener("click", e =>{
+document.getElementById("hint3-b").addEventListener("click", e =>{
   textHint.innerText = "A tinta desses canetões só não é parea para a próxima dica!";
   modal.style.display = "flex";
 })      
-document.getElementById("pista3c").addEventListener("click", e =>{
+document.getElementById("hint3-c").addEventListener("click", e =>{
   textHint.innerText = "A tinta desses canetões só não é parea para a próxima dica!";
   modal.style.display = "flex";
 })
 
-document.getElementById("pista4").addEventListener("click", e =>{
+document.getElementById("hint4").addEventListener("click", e =>{
   textHint.innerText = "Tenho medo de fogo!";
   modal.style.display = "flex";
 })
 
-document.getElementById("resultadoa").addEventListener("click", e =>{
+document.getElementById("finished-a").addEventListener("click", e =>{
   textHint.innerText = "Tenho medo de fogo!";
   modal.style.display = "flex";
 })      
-document.getElementById("resultadob").addEventListener("click", e =>{
+document.getElementById("finished-b").addEventListener("click", e =>{
   textHint.innerText = "Tenho medo de fogo!";
   modal.style.display = "flex";
 })

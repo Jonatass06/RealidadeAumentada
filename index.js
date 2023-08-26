89b219c9186d070469a806319b8233610113856b
Jonatass06 <116910385+Jonatass06@users.noreply.github.com>
2023-08-26 15:03:08 +0000

    ,

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ document.getElementById("hint").addEventListener("click", e => {
 document.getElementById("hint1").addEventListener("click", e => {
   textHint.innerText = "Procure o equilíbrio. Ajuste o conforto com as correntes de ar, nem muito frias, tampouco quentes.";
   modal.style.display = "flex";
-  aEntities.innerHTML += `
-  <a-entity id="control" mindar-image-target="targetIndex: 1" class="hidden">
-  <a-gltf-model id="hint2"  scale="0.001 0.001 0.001" position="0 0 0" rotation="0 0 0" src="#quest" class="clickable" animation-mixer>
-  </a-entity>`;
+  aEntities.innerHTML += '<a-entity id="control" mindar-image-target="targetIndex: 1" class="hidden"><a-gltf-model id="hint2"  scale="0.001 0.001 0.001" position="0 0 0" rotation="0 0 0" src="#quest" class="clickable" animation-mixer></a-entity>'s;
 })
 
 document.getElementById("hint2").addEventListener("click", e => {

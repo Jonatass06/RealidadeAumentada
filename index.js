b8a744409fab6b54d44bb8e8186a4f7f38f58102
Jonatass06 <116910385+Jonatass06@users.noreply.github.com>
2023-08-26 13:53:19 +0000

    Escondendo dicas

--- index.js
+++ index.js
@@ -15,28 +15,39 @@ document.getElementById("hint").addEventListener("click", e =>{
 document.getElementById("hint1").addEventListener("click", e =>{
   textHint.innerText = "Procure o equilíbrio. Ajuste o conforto com as correntes de ar, nem muito frias, tampouco quentes.";
   modal.style.display = "flex";
+  document.getElementById("control").classList.remove("hidden")
 })
 
 document.getElementById("hint2").addEventListener("click", e =>{
   textHint.innerText = "A chave está na criatividade. Cores traçam pistas invisíveis, guiando você com marcas deixadas em algo branco.";
   modal.style.display = "flex";
+  document.getElementById("pilot1").classList.remove("hidden")
+  document.getElementById("pilot2").classList.remove("hidden")
+  document.getElementById("pilot3").classList.remove("hidden")
+
 })
 
 document.getElementById("hint3-a").addEventListener("click", e =>{
   textHint.innerText = "Desvende mistérios ao desfazer os traços. O poder transcende a superfície, apagando o que foi desenhado";
   modal.style.display = "flex";
+  document.getElementById("eraser").classList.remove("hidden")
+
 })
 document.getElementById("hint3-b").addEventListener("click", e =>{
   textHint.innerText = "Desvende mistérios ao desfazer os traços. O poder transcende a superfície, apagando o que foi desenhado";
+  document.getElementById("eraser").classList.remove("hidden")
   modal.style.display = "flex";
 })      
 document.getElementById("hint3-c").addEventListener("click", e =>{
   textHint.innerText = "Desvende mistérios ao desfazer os traços. O poder transcende a superfície, apagando o que foi desenhado";
+  document.getElementById("eraser").classList.remove("hidden")
   modal.style.display = "flex";
 })
 
 document.getElementById("hint4").addEventListener("click", e =>{
   textHint.innerText = "Siga o caminho da água. Como o ser que sou, adoro frio, odeio o calor, o fogo.";
+  document.getElementById("fire1").classList.remove("hidden")
+  document.getElementById("fire2").classList.remove("hidden")
   modal.style.display = "flex";
 })
 

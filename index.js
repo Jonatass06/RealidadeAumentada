d56921903d787e8c7e01d25ee7f62258826c0f1d
Jonatass06 <116910385+Jonatass06@users.noreply.github.com>
2023-08-27 23:08:44 +0000

    a

--- index.js
+++ index.js
@@ -7,53 +7,51 @@ const sceneEl = document.querySelector('a-scene');
 const arSystem = sceneEl.systems["mindar-image-system"];
 textHint.innerText = "Olá seja bem vindo ao nosso caça ao tesouro, comece respirando um pouco!"
 
-function closeModal(element){
-    modal.style.display = "none";
-    arSystem.start();
-  }
-  
-  document.getElementById("hint").addEventListener("click", e => {
-    modal.style.display = "flex";
-  })
-  
-  document.getElementById("hint1").addEventListener("click", e => {
-    modal.style.display = "flex";
-    aEntities.innerHTML += '<a-entity id="control" mindar-image-target="targetIndex: 1" class="hidden"><a-gltf-model id="hint2"  scale="0.001 0.001 0.001" position="0 0 0" rotation="0 0 0" src="#quest" class="clickable" animation-mixer></a-entity>';
-    textHint.innerText = "Procure o equilíbrio. Ajuste o conforto com as correntes de ar, nem muito frias, tampouco quentes.";
-    arSystem.stop();
+function closeModal(element) {
+  modal.style.display = "none";
+  arSystem.start();
+}
 
-  })
+document.getElementById("hint").addEventListener("click", e => {
+  modal.style.display = "flex";
+})
+
+document.getElementById("hint1").addEventListener("click", e => {
+  modal.style.display = "flex";
+  aEntities.innerHTML += '<a-entity id="control" mindar-image-target="targetIndex: 1" class="hidden"><a-gltf-model id="hint2"  scale="0.001 0.001 0.001" position="0 0 0" rotation="0 0 0" src="#quest" class="clickable" animation-mixer></a-entity>';
+  textHint.innerText = "Procure o equilíbrio. Ajuste o conforto com as correntes de ar, nem muito frias, tampouco quentes.";
+  arSystem.stop();
+})
+
+document.getElementById("hint1").addEventListener("click", e => {
+  textHint.innerText = "Procure o equilíbrio. Ajuste o conforto com as correntes de ar, nem muito frias, tampouco quentes.";
+  modal.style.display = "flex";
+  document.getElementById("control").classList.remove("hidden")
+})
 
 document.getElementById("hint2").addEventListener("click", e => {
+
   textHint.innerText = "A chave está na criatividade. Cores traçam pistas invisíveis, guiando você com marcas deixadas em algo branco.";
   modal.style.display = "flex";
-  document.getElementById("pilot1").classList.remove("hidden")
-  document.getElementById("pilot2").classList.remove("hidden")
-  document.getElementById("pilot3").classList.remove("hidden")
 
 })
 
 document.getElementById("hint3-a").addEventListener("click", e => {
   textHint.innerText = "Desvende mistérios ao desfazer os traços. O poder transcende a superfície, apagando o que foi desenhado";
   modal.style.display = "flex";
-  document.getElementById("eraser").classList.remove("hidden")
 
 })
 document.getElementById("hint3-b").addEventListener("click", e => {
   textHint.innerText = "Desvende mistérios ao desfazer os traços. O poder transcende a superfície, apagando o que foi desenhado";
-  document.getElementById("eraser").classList.remove("hidden")
   modal.style.display = "flex";
 })
 document.getElementById("hint3-c").addEventListener("click", e => {
   textHint.innerText = "Desvende mistérios ao desfazer os traços. O poder transcende a superfície, apagando o que foi desenhado";
-  document.getElementById("eraser").classList.remove("hidden")
   modal.style.display = "flex";
 })
 
 document.getElementById("hint4").addEventListener("click", e => {
   textHint.innerText = "Siga o caminho da água. Como o ser que sou, adoro frio, odeio o calor, o fogo.";
-  document.getElementById("fire1").classList.remove("hidden")
-  document.getElementById("fire2").classList.remove("hidden")
   modal.style.display = "flex";
 })
 

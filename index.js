008c6db595f867af3533619ba580ccf50ed0ad1a
Jonatass06 <116910385+Jonatass06@users.noreply.github.com>
2023-08-28 00:16:39 +0000

    A

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ document.getElementById("hint").addEventListener("click", e => {
 
 document.getElementById("hint1").addEventListener("click", e => {
   document.querySelector("body").removeChild(cena1);
+  document.querySelector("body").innerHTML += '<a-scene mindar-image="imageTargetSrc: ./targets.mind; autoStart: false;" color-space="sRGB"renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false"device-orientation-permission-ui="enabled: false" id="cena2"><a-camera position="0 0 0" look-controls="enabled: false" cursor="fuse: false; rayOrigin: mouse;"raycaster="far: ${customFields.libVersion}; objects: .clickable"></a-camera><a-entity id="control" mindar-image-target="targetIndex: 1" class="hidden"><a-gltf-model id="hint2" scale="0.001 0.001 0.001" position="0 0 0" rotation="0 0 0" src="#quest" class="clickable" animation-mixer></a-entity></a-scene>'
   modal.style.display = "flex";
   alert("a");
   textHint.innerText = "Procure o equilíbrio. Ajuste o conforto com as correntes de ar, nem muito frias, tampouco quentes.";
